86f23c6890161770e3861b7784995f7f7295e62f
gmachara7-tech <gmachara7@gmail.com>
2026-05-29 22:56:59 +0400

    დავალება_2

---
+++ დავალება_2/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace დავალება_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sheiyvanet pirveli ricxvi");
+            int first = int.Parse(Console.ReadLine());
+            Console.WriteLine("Sheiyvanet me-2 ricxvi");
+            int second = int.Parse(Console.ReadLine());
+            Console.WriteLine("Sheiyvanet me-3 ricxvi");
+            int third = int.Parse(Console.ReadLine());
+            int big = first;
+            if (second > big)
+            {
+                big = second;
+            }
+            if (third > big)
+            {
+                big = third;
+            }
+            Console.WriteLine("Udidesi ricxvia" + big);
+
+            Console.ReadLine();
+        }
+    }
+}
