76190dc6f31e33767a88e7898edf8e772d0e2c56
gmachara7-tech <gmachara7@gmail.com>
2026-05-29 22:21:03 +0400

    Add project files.

---
+++ დავალება 2/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace დავალება_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Sheiyvanet amomrchevelis asaki: ");
+            int asaki = int.Parse(Console.ReadLine());
+            if (asaki >= 18)
+            {
+                Console.WriteLine("Xmis micemis ufleba gaqvt");
+            }
+            else
+            {
+                Console.WriteLine("samwucarod jer ver miscemt xmas dabrundit roca iqnebit 18 is");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
