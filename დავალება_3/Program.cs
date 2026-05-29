d59dc07b43d4452c603726d59da7fe6c5769f3f4
gmachara7-tech <gmachara7@gmail.com>
2026-05-29 23:28:50 +0400

    დავალება_3

---
+++ დავალება_3/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace დავალება_3
+{
+    internal class Program
+    {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Sheiyvanet pirveli ricxvi");
+                int num1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Sheiyvanet me-2 ricxvi:");
+                int num2 = int.Parse(Console.ReadLine());
+                int jami = num1 + num2;
+                if (num1 == num2)
+                {
+                    jami = jami * 3;
+                }
+                Console.WriteLine("Shedegi: " + jami);
+
+                Console.ReadLine();
+            }
+        }
+    }
+    
